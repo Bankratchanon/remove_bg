7c03f9e318b9822da9984f35762da0aaf73aebba
Bankratchanon <ratchanonpaenchan@gmail.com>
2026-07-20 15:05:47 +0700

    fix contact

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ async function removeBackground() {
     showNotification('Please upload an image or enter a URL first.');
     return;
   }
-  const apiKey = 'uFPDEptpd8HxmGo8gnZKAer9';
+  const apiKey = 'edZivE3tX4xVZTWAsmiccVXK'; //uFPDEptpd8HxmGo8gnZKAer9
 
   try {
     const response = await fetch('https://api.remove.bg/v1.0/removebg', {
